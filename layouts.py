8d35a6498b21aadd0116d7aa8346fc11d38a4e7d
meriusss <mariusginghina1@gmail.com>
2023-11-03 00:41:41 +0200

    added cpu and memory data

--- layouts.py
+++ layouts.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
+from monitor import *
 
 WindowBackgroundColor = "#9BBEC8"
 ButtonColor = "#427D9D"
 ButtonColorActive = "#195170"
+ButtonTextColor = "#FFFFFF"
 
 def Navigation_Button(title):
-    return sg.Button(title, size = (12,2), tooltip = title, button_color = ("#FFFFFF", ButtonColor) , mouseover_colors = "#164863", border_width = 0)
+    return sg.Button(title, size = (12,2), tooltip = title, button_color = (ButtonTextColor, ButtonColor) , mouseover_colors = "#164863", border_width = 0)
+
+def Component_Data_Layout(Title, Data):
+    return [sg.Text(Title), sg.Push(), sg.Text(Data)]
 
 ButtonLayout = [Navigation_Button("CPU"), Navigation_Button("Memory"), Navigation_Button("GPU"), Navigation_Button("Storage"), Navigation_Button("Network")]
 
@@ -19,23 +24,89 @@ NavigationLayout = sg.Frame(
             )
 
 layoutCPU = [
-    [sg.Text("CPU")]
+    [   
+        sg.Frame("", layout = [
+            Component_Data_Layout("CPU", Get_CPU_Name()),
+            Component_Data_Layout("Physical CPU Cores", Get_CPU_Cores()),
+            Component_Data_Layout("Logical CPU Cores", Get_CPU_Cores(True)),
+            Component_Data_Layout("Base Frequency", Get_CPU_Base_Freq()),
+            Component_Data_Layout("Architecture", Get_CPU_Architecture())
+        ],
+        size = (555, 232))
+    ],
+
+    [
+        sg.Frame("", layout = [
+            [
+                sg.Text("CPU Activity")
+            ]
+        ],
+        size = (555, 232))
+    ]
 ]
 
 layoutMemory = [
-    [sg.Text("Memory")]
+    [   
+        sg.Frame("", layout = [
+            Component_Data_Layout("Total Available Memory", Get_Total_Available_Memory("GB"))
+        ],
+        size = (555, 232))
+    ],
+
+    [
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ]
 ]
 
 layoutGPU = [
-    [sg.Text("GPU")]
+    [   
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ],
+
+    [
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ]
 ]
 
 layoutStorage = [
-    [sg.Text("Storage")]
+    [   
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ],
+
+    [
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ]
 ]
 
 layoutNetwork = [
-    [sg.Text("Network")]
+    [   
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ],
+
+    [
+        sg.Frame("", layout = [
+            
+        ],
+        size = (555, 232))
+    ]
 ]
 
 layout = [
@@ -44,19 +115,19 @@ layout = [
     ],
 
     [
-        sg.Column(layoutCPU, key = 'CPUL', size = (600, 475)), 
-        sg.Column(layoutMemory, key = "MemoryL", visible = False, size = (600, 475)), 
-        sg.Column(layoutGPU, key = "GPUL", visible = False, size = (600, 475)), 
-        sg.Column(layoutStorage, key = "StorageL", visible = False, size = (600, 475)), 
-        sg.Column(layoutNetwork, key = "NetworkL", visible = False, size = (600, 475))
+        sg.Column(layoutCPU, key = 'CPUL', size = (600, 480)), 
+        sg.Column(layoutMemory, key = "MemoryL", visible = False, size = (600, 480)), 
+        sg.Column(layoutGPU, key = "GPUL", visible = False, size = (600, 480)),
+        sg.Column(layoutStorage, key = "StorageL", visible = False, size = (600, 480)), 
+        sg.Column(layoutNetwork, key = "NetworkL", visible = False, size = (600, 480))
     ],
 
     [
         sg.Frame("", layout = [
             [
-                sg.B("", image_filename = "images/github.png", image_size = (26, 26), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key = "-github-"),
-                sg.B("", image_filename = "images/settings.png", image_size = (26, 26), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key = "-settings-"),
-                sg.B("", image_filename = "images/exit.png", image_size = (26, 26), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key='-exit-')
+                sg.B("", image_filename = "images/github.png", image_size = (30, 30), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key = "-github-"),
+                sg.B("", image_filename = "images/settings.png", image_size = (30, 30), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key = "-settings-"),
+                sg.B("", image_filename = "images/exit.png", image_size = (30, 30), border_width = 0, button_color = WindowBackgroundColor, mouseover_colors = WindowBackgroundColor, key='-exit-')
             ]
         ],
         size = (600, 50),
