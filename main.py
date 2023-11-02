2f8e9aaf78e2775418e4cd3018bf218fdef92894
meriusss <mariusginghina1@gmail.com>
2023-11-03 00:42:23 +0200

    added layout change function

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import PySimpleGUI as sg
 from layouts import *
 import webbrowser
 
-window = sg.Window('System Monitor', layout, size = (600, 600), background_color = WindowBackgroundColor, icon = "images/icon.ico", element_justification = "center", no_titlebar = True, grab_anywhere = True)
+window = sg.Window('System Monitor', layout, size = (600, 600), background_color = WindowBackgroundColor, icon = "images/icon.ico", element_justification = "center", no_titlebar = False, grab_anywhere = True)
 
 layout = "CPUL"
+
+def Change_Layout(LayoutToChangeTo):
+    global layout
+    window[layout].update(visible = False)
+    layout = LayoutToChangeTo
+    window[layout].update(visible = True)
 
 while True:
     
@@ -15,25 +21,15 @@ while True:
     if event == "-github-":
         webbrowser.open("https://github.com/meriusss", new = 2)
     if event == "CPU":
-        window[layout].update(visible = False)
-        layout = "CPUL"
-        window[layout].update(visible = True)
+        Change_Layout("CPUL")
     if event == "Memory":
-        window[layout].update(visible = False)
-        layout = "MemoryL"
-        window[layout].update(visible = True)
+        Change_Layout("MemoryL")
     if event == "GPU":
-        window[layout].update(visible = False)
-        layout = "GPUL"
-        window[layout].update(visible = True)
+        Change_Layout("GPUL")
     if event == "Storage":
-        window[layout].update(visible = False)
-        layout = "StorageL"
-        window[layout].update(visible = True)
+        Change_Layout("StorageL")
     if event == "Network":
-        window[layout].update(visible = False)
-        layout = "NetworkL"
-        window[layout].update(visible = True)
+        Change_Layout("NetworkL")
 
         
 window.close()
