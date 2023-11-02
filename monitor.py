72e02217d6bf522c06af0fa2a48d100230aadd8f
meriusss <mariusginghina1@gmail.com>
2023-11-03 00:44:50 +0200

    added functions for CPU architecture, CPU frequency and total available memory

--- monitor.py
+++ monitor.py
@@ -9,3 +9,17 @@ def Get_CPU_Cores(logical = False):
 
 def Get_CPU_Usage():
     return psutil.cpu_percent(interval= None)
+
+def Get_CPU_Base_Freq():
+    return cpuinfo.get_cpu_info()["hz_advertised_friendly"]
+
+def Get_CPU_Architecture():
+    return cpuinfo.get_cpu_info()["arch"]
+
+def Get_Total_Available_Memory(unit):
+    if unit == "GB":
+        return str(round(psutil.virtual_memory().total / 1024 / 1024 / 1024, 2)) + " " + unit
+    elif unit == "MB":
+        return str(round(psutil.virtual_memory().total / 1024 / 1024)) + " " + unit
+    else: 
+        return str(round(psutil.virtual_memory().total / 1024 / 1024 / 1024, 2)) + " " + unit
